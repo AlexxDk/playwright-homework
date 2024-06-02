5cafa124298d060ca8e60227c7272e952453e9c4
AlexxDk <dkalexandr@gmail.com>
2024-06-03 00:10:50 +0300

    add all steps for 1 test cases

--- tests/inputFields.spec.ts
+++ tests/inputFields.spec.ts
@@ -21,11 +21,31 @@ test("Update pet type", async ({ page }) => {
 
   await expect(page.locator("h2")).toHaveText("Edit Pet Type");
 
-  const petTypeInput = page.locator('[name="name"]');
+  const petTypeInput = page.locator("#name");
+  await petTypeInput.click();
   await petTypeInput.clear();
-  await petTypeInput.pressSequentially('rabbit', {delay: 500});
-  await page.getByRole('button', {name: 'Update'}).click()
-  
-  const firstPetTypeInTable = page.locator('td', {has: page.locator('[name="pettype_name"]')}).first()
-  await expect(firstPetTypeInTable).toHaveValue('rabbit')
+
+  //   await page.waitForTimeout(5000);
+  await petTypeInput.fill("rabbit");
+  await page.getByRole("button", { name: "Update" }).click();
+
+  await page.waitForSelector('[id="0"]');
+  const firstPetTypeInTable = page
+    .locator('input[name="pettype_name"]')
+    .first();
+  await expect(firstPetTypeInTable).toHaveValue("rabbit");
+
+  for (let row of await tableRows.all()) {
+    const cellValue = await row.locator("input").inputValue();
+    if (cellValue === "rabbit") {
+      await row.getByRole("button", { name: "Edit" }).click();
+      break;
+    }
+  }
+  await petTypeInput.click();
+  await petTypeInput.clear();
+  await petTypeInput.fill("cat");
+  await page.getByRole("button", { name: "Update" }).click();
+
+  await expect(firstPetTypeInTable).toHaveValue("cat");
 });
