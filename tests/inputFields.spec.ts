9a34f5fc7d6f015f2a51c5649d48808c9938c63a
AlexxDk <dkalexandr@gmail.com>
2024-06-03 15:27:26 +0300

    add 2, 3 test cases (cancel and validation flow)

--- tests/inputFields.spec.ts
+++ tests/inputFields.spec.ts
@@ -2,13 +2,13 @@ import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/");
-});
 
-test("Update pet type", async ({ page }) => {
   await page.getByText(" Pet Types").click();
   const tableTitle = await page.locator("h2").textContent();
   expect(tableTitle).toEqual("Pet Types");
+});
 
+test("Update pet type", async ({ page }) => {
   const tableRows = page.locator("tbody tr");
   await page.waitForSelector('[id="0"]');
   for (let row of await tableRows.all()) {
@@ -48,4 +48,58 @@ test("Update pet type", async ({ page }) => {
   await page.getByRole("button", { name: "Update" }).click();
 
   await expect(firstPetTypeInTable).toHaveValue("cat");
+});
+
+test("Cancel pet type update", async ({ page }) => {
+  const tableRows = page.locator("tbody tr");
+  await page.waitForSelector('[id="0"]');
+  for (let row of await tableRows.all()) {
+    const cellValue = await row.locator("input").inputValue();
+    if (cellValue === "dog") {
+      await row.getByRole("button", { name: "Edit" }).click();
+      break;
+    }
+  }
+
+  const petTypeInput = page.locator("#name");
+  await petTypeInput.click();
+  await petTypeInput.clear();
+  await petTypeInput.fill("moose");
+
+  await expect(petTypeInput).toHaveValue("moose");
+
+  await page.getByRole("button", { name: "Cancel" }).click();
+
+  await page.waitForSelector('[id="0"]');
+  const secondPetTypeInTable = page
+    .locator('input[name="pettype_name"]')
+    .nth(1);
+  await expect(secondPetTypeInTable).toHaveValue("dog");
+});
+
+test("Pet type name is required validation", async ({ page }) => {
+  const tableRows = page.locator("tbody tr");
+  await page.waitForSelector('[id="0"]');
+  for (let row of await tableRows.all()) {
+    const cellValue = await row.locator("input").inputValue();
+    if (cellValue === "lizard") {
+      await row.getByRole("button", { name: "Edit" }).click();
+      break;
+    }
+  }
+
+  const petTypeInput = page.locator("#name");
+  await petTypeInput.click();
+  await petTypeInput.clear();
+
+  const validationMessage = await petTypeInput
+    .locator("..")
+    .locator(".help-block")
+    .textContent();
+  expect(validationMessage).toEqual("Name is required");
+
+  await page.getByRole("button", { name: "Cancel" }).click();
+
+  const tableTitle = await page.locator("h2").textContent();
+  expect(tableTitle).toEqual("Pet Types");
 });
