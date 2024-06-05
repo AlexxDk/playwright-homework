fa7e57d1744939587b73a75a8a3ca0d888f04cdf
AlexxDk <dkalexandr@gmail.com>
2024-06-05 20:42:55 +0300

    fixes after review

--- tests/inputFields.spec.ts
+++ tests/inputFields.spec.ts
@@ -4,20 +4,14 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/");
 
   await page.getByText(" Pet Types").click();
-  const tableTitle = await page.locator("h2").textContent();
-  expect(tableTitle).toEqual("Pet Types");
+  await expect(page.locator("h2")).toHaveText("Pet Types");
 });
 
 test("Update pet type", async ({ page }) => {
-  const tableRows = page.locator("tbody tr");
-  await page.waitForSelector('[id="0"]');
-  for (let row of await tableRows.all()) {
-    const cellValue = await row.locator("input").inputValue();
-    if (cellValue === "cat") {
-      await row.getByRole("button", { name: "Edit" }).click();
-      break;
-    }
-  }
+  const catPetTypeInTable = page.locator("tr", {
+    has: page.locator('[id="0"]'),
+  });
+  await catPetTypeInTable.getByRole("button", { name: "Edit" }).click();
 
   await expect(page.locator("h2")).toHaveText("Edit Pet Type");
 
@@ -25,41 +19,26 @@ test("Update pet type", async ({ page }) => {
   await petTypeInput.click();
   await petTypeInput.clear();
 
-  //   await page.waitForTimeout(5000);
   await petTypeInput.fill("rabbit");
   await page.getByRole("button", { name: "Update" }).click();
 
-  await page.waitForSelector('[id="0"]');
-  const firstPetTypeInTable = page
-    .locator('input[name="pettype_name"]')
-    .first();
-  await expect(firstPetTypeInTable).toHaveValue("rabbit");
+  await expect(catPetTypeInTable.locator("input")).toHaveValue("rabbit");
 
-  for (let row of await tableRows.all()) {
-    const cellValue = await row.locator("input").inputValue();
-    if (cellValue === "rabbit") {
-      await row.getByRole("button", { name: "Edit" }).click();
-      break;
-    }
-  }
+  await catPetTypeInTable.getByRole("button", { name: "Edit" }).click();
+
   await petTypeInput.click();
   await petTypeInput.clear();
   await petTypeInput.fill("cat");
   await page.getByRole("button", { name: "Update" }).click();
 
-  await expect(firstPetTypeInTable).toHaveValue("cat");
+  await expect(catPetTypeInTable.locator("input")).toHaveValue("cat");
 });
 
 test("Cancel pet type update", async ({ page }) => {
-  const tableRows = page.locator("tbody tr");
-  await page.waitForSelector('[id="0"]');
-  for (let row of await tableRows.all()) {
-    const cellValue = await row.locator("input").inputValue();
-    if (cellValue === "dog") {
-      await row.getByRole("button", { name: "Edit" }).click();
-      break;
-    }
-  }
+  const dogPetTypeInTable = page.locator("tr", {
+    has: page.locator('[id="1"]'),
+  });
+  await dogPetTypeInTable.getByRole("button", { name: "Edit" }).click();
 
   const petTypeInput = page.locator("#name");
   await petTypeInput.click();
@@ -70,23 +49,14 @@ test("Cancel pet type update", async ({ page }) => {
 
   await page.getByRole("button", { name: "Cancel" }).click();
 
-  await page.waitForSelector('[id="0"]');
-  const secondPetTypeInTable = page
-    .locator('input[name="pettype_name"]')
-    .nth(1);
-  await expect(secondPetTypeInTable).toHaveValue("dog");
+  await expect(dogPetTypeInTable.locator("input")).toHaveValue("dog");
 });
 
 test("Pet type name is required validation", async ({ page }) => {
-  const tableRows = page.locator("tbody tr");
-  await page.waitForSelector('[id="0"]');
-  for (let row of await tableRows.all()) {
-    const cellValue = await row.locator("input").inputValue();
-    if (cellValue === "lizard") {
-      await row.getByRole("button", { name: "Edit" }).click();
-      break;
-    }
-  }
+  const lizardPetTypeInTable = page.locator("tr", {
+    has: page.locator('[id="2"]'),
+  });
+  await lizardPetTypeInTable.getByRole("button", { name: "Edit" }).click();
 
   const petTypeInput = page.locator("#name");
   await petTypeInput.click();
@@ -100,6 +70,5 @@ test("Pet type name is required validation", async ({ page }) => {
 
   await page.getByRole("button", { name: "Cancel" }).click();
 
-  const tableTitle = await page.locator("h2").textContent();
-  expect(tableTitle).toEqual("Pet Types");
+  await expect(page.locator("h2")).toHaveText("Pet Types");
 });
