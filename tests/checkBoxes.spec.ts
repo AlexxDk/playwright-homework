b7af4a1faae099aa776a185037464246db0b2cfe
AlexxDk <dkalexandr@gmail.com>
2024-06-09 19:40:55 +0300

    add 2nd test case: Select all specialties

--- tests/checkBoxes.spec.ts
+++ tests/checkBoxes.spec.ts
@@ -34,3 +34,26 @@ test("Validate selected specialties", async ({ page }) => {
 
   await expect(selectedSpecialtiesDropdown).toContainText("surgery, dentistry");
 });
+
+test("Select all specialties", async ({ page }) => {
+  const veterinarianRafael = page.locator("tr", {
+    has: page.getByText(" Rafael Ortega "),
+  });
+  await veterinarianRafael.getByRole("button", { name: "Edit Vet" }).click();
+
+  const selectedSpecialtiesDropdown = page.locator(
+    ".dropdown-display .selected-specialties"
+  );
+  await expect(selectedSpecialtiesDropdown).toContainText("surgery");
+  await selectedSpecialtiesDropdown.click();
+
+  const allSpecialtiesCheckboxes = page.getByRole("checkbox");
+  for (const box of await allSpecialtiesCheckboxes.all()) {
+    await box.check();
+    expect(await box.isChecked()).toBeTruthy();
+  }
+
+  await expect(selectedSpecialtiesDropdown).toContainText(
+    "surgery, radiology, dentistry, new specialty "
+  );
+});
