e04f3c387e7a227658bea9e75670fa65598950b1
AlexxDk <dkalexandr@gmail.com>
2024-06-09 19:14:03 +0300

    add 1st test case: Validate selected specialties

---
+++ tests/checkBoxes.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+
+  await page.getByText(" Veterinarians").click();
+  await page.locator(".dropdown-menu").getByText(" All").click();
+});
+
+test("Validate selected specialties", async ({ page }) => {
+  await expect(page.locator("h2")).toHaveText("Veterinarians");
+
+  const veterinarianHelen = page.locator("tr", {
+    has: page.getByText(" Helen Leary "),
+  });
+  await veterinarianHelen.getByRole("button", { name: "Edit Vet" }).click();
+
+  const selectedSpecialtiesDropdown = page.locator(
+    ".dropdown-display .selected-specialties"
+  );
+  await expect(selectedSpecialtiesDropdown).toContainText("radiology");
+  await selectedSpecialtiesDropdown.click();
+
+  expect(await page.locator("#radiology").isChecked()).toBeTruthy();
+  expect(await page.locator("#surgery").isChecked()).toBeFalsy();
+  expect(await page.locator("#dentistry").isChecked()).toBeFalsy();
+
+  await page.locator("#surgery").check();
+  await page.locator("#radiology").uncheck();
+
+  await expect(selectedSpecialtiesDropdown).toContainText("surgery");
+
+  await page.locator("#dentistry").check();
+
+  await expect(selectedSpecialtiesDropdown).toContainText("surgery, dentistry");
+});
