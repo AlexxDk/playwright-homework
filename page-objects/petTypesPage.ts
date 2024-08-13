ba7f9e7b9ea0ba68cd7f1d5e56d7ec9788ba7ca3
AlexxDk <dkalexandr@gmail.com>
2024-08-14 01:41:16 +0300

    update methods for petTypesPage

--- page-objects/petTypesPage.ts
+++ page-objects/petTypesPage.ts
@@ -19,4 +19,32 @@ export class PetTypesPage {
         await expect(this.page.locator("tr", { has: this.page.locator(`[id="${index}"]`), })
             .locator("input")).toHaveValue(updatedPetName);
     }
+
+    async openAddNewPetTypeForm() {
+        await this.page.locator('.btn-default', { hasText: " Add " }).click()
+        await expect(this.page.locator('.xd-container h2').last()).toHaveText('New Pet Type')
+        const addNewPetForm = this.page.locator('.form-group')
+        const inputNewPetType = addNewPetForm.locator('#name')
+        await expect(addNewPetForm.locator('label')).toHaveText("Name")
+        await expect(inputNewPetType).toBeVisible()
+        return inputNewPetType
+    }
+
+    async fillFormAndSaveNewPetType(name: string) {
+        await this.page.locator('.form-group').locator('#name').fill(name)
+        await this.page.getByRole('button', { name: "Save" }).click()
+    }
+
+    async deleteLastPetType(name: string) {
+        const lastInput = this.page.locator('[name="pettype_name"]').last()
+        await expect(lastInput).toHaveValue(name)
+
+        this.page.on('dialog', dialog => {
+            expect(dialog.message()).toEqual('Delete the pet type?')
+            dialog.accept()
+        })
+
+        await this.page.getByRole("button", { name: "Delete" }).last().click();
+        await expect(lastInput).not.toHaveValue(name)
+    }
 }
