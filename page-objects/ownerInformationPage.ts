a42c0b3ea09b39b419ddf2811134b77f3e4f01cc
AlexxDk <dkalexandr@gmail.com>
2024-08-19 23:03:16 +0300

    test cases with mocked data

--- page-objects/ownerInformationPage.ts
+++ page-objects/ownerInformationPage.ts
@@ -77,4 +77,8 @@ export class OwnerInformationPage extends HelperBase {
     private returnVisitRowByPetAndDescription(petName: string, description: string) {
         return this.page.locator('app-pet-list', { has: this.page.getByText(petName) }).locator('app-visit-list').locator('tr', { has: this.page.getByText(description) })
     }
+
+    async returnTotalVisit(petName: string) {
+        return this.page.locator('app-pet-list', { has: this.page.getByText(petName) }).getByRole('button', { name: "Edit Visit" })
+    }
 }
