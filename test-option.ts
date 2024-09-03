066b4aed0e13f3bb60ed4d38eb40f4ad5af1a49c
AlexxDk <dkalexandr@gmail.com>
2024-09-04 00:19:01 +0300

    adjust test fixtures

--- test-option.ts
+++ test-option.ts
@@ -1,15 +1,17 @@
-import { test as base } from '@playwright/test'
+import { test as base, expect } from '@playwright/test'
 import { PageManager } from './page-objects/pageManager'
 import { TestDataGeneration } from "./test-data/TestDataGeneration";
 
-
 export type TestOptions = {
-    ownerWithPetAndVisitSetup: string
+    ownerWithPetAndVisitSetup: {
+        petName: string;
+        visitDescription: string;
+    }
     pageManager: PageManager
 }
 
 export const test = base.extend<TestOptions>({
-    ownerWithPetAndVisitSetup: async ({ page }, use) => {
+    ownerWithPetAndVisitSetup: async ({ page, request }, use) => {
         const randomOwnerFirstName = TestDataGeneration.randomOwnerFirstName()
         const randomOwnerLastName = TestDataGeneration.randomOwnerLastName()
         const randomOwnerAddress = TestDataGeneration.randomOwnerAddress()
@@ -23,23 +25,32 @@ export const test = base.extend<TestOptions>({
             birthDay: '02'
         }
         let date = new Date();
-        
-        await page.goto("/");   
-        // const pm = new PageManager(page)
-        // await pm.navigateTo().ownersPage()
-        // await pm.onOwnersPage().openAddNewOwner()
-        // await pm.onAddNewOwnerPage().addOwner(randomOwnerFirstName, randomOwnerLastName, randomOwnerAddress, randomOwnerCity, randomOwnerPhoneNumber)
-        // await pm.onOwnersPage().openOwnerInfoPageFor(`${randomOwnerFirstName} ${randomOwnerLastName}`)
-        // await pm.onOwnerInformationPage().openFormAddNewPet()
-        // await pm.onAddNewPetPage().addPet(randomPetName, birthDay, birthMonth, birthYear, 'dog')
-        // await pm.onOwnerInformationPage().openFormAddVisit(randomPetName, `${randomOwnerFirstName} ${randomOwnerLastName}`)
-        // await pm.onAddNewVisitPage().addVisitForToday(firstRandomDescriptionForThePetVisit, date)
-        // await pm.onOwnerInformationPage().validateVisitDateForPet(randomPetName, date)
 
-        await use('')
+        await page.goto("/");
+        const pm = new PageManager(page)
+        await pm.navigateTo().ownersPage()
+        await pm.onOwnersPage().openAddNewOwner()
+        await pm.onAddNewOwnerPage().addOwner(randomOwnerFirstName, randomOwnerLastName, randomOwnerAddress, randomOwnerCity, randomOwnerPhoneNumber)
+        const ownersResponse = await page.waitForResponse('https://petclinic-api.bondaracademy.com/petclinic/api/owners')
+        const ownersResponseBody = await ownersResponse.json()
+        const ownerID = ownersResponseBody.id
+        await pm.onOwnersPage().openOwnerInfoPageFor(`${randomOwnerFirstName} ${randomOwnerLastName}`)
+        await pm.onOwnerInformationPage().openFormAddNewPet()
+        await pm.onAddNewPetPage().addPet(randomPetName, birthDay, birthMonth, birthYear, 'dog')
+        await pm.onOwnerInformationPage().openFormAddVisit(randomPetName, `${randomOwnerFirstName} ${randomOwnerLastName}`)
+        await pm.onAddNewVisitPage().addVisitForToday(firstRandomDescriptionForThePetVisit, date)
+        await pm.onOwnerInformationPage().validateVisitDateForPet(randomPetName, date)
+
+        await use({ petName: randomPetName, visitDescription: firstRandomDescriptionForThePetVisit })
+
+        const deleteOwnerResponse = await request.delete(`https://petclinic-api.bondaracademy.com/petclinic/api/owners/${ownerID}`, {
+            headers: { Authorization: `Bearer ${process.env.ACCESS_TOKEN}` }
+        })
+        expect(deleteOwnerResponse.status()).toEqual(204)
+
     },
 
-    pageManager: async ({ page, ownerWithPetAndVisitSetup}, use) => {
+    pageManager: async ({ page, ownerWithPetAndVisitSetup }, use) => {
         const pm = new PageManager(page)
         await use(pm)
     }
--- tests/testWithFixture.spec.ts
+++ tests/testWithFixture.spec.ts
@@ -1,5 +1,7 @@
 import { test } from "../test-option";
 
-test('test with fixture', async ({ pageManager }) => {
-    await pageManager.onOwnerInformationPage().deleteVisitForPet('petname', 'firstRandomDescriptionForThePetVisit')
+test('test with fixture', async ({ ownerWithPetAndVisitSetup, pageManager }) => {
+    const { petName, visitDescription } = ownerWithPetAndVisitSetup;
+    await pageManager.onOwnerInformationPage().deleteVisitForPet(petName, visitDescription)
+    await pageManager.onOwnerInformationPage().deletePetByName(petName)
 })
